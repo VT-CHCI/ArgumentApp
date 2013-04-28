f8c165c0d4e1ffcd31b059a48b7193ea21c0a1bd
mil <miles.sandlar@gmail.com>
2013-04-28 18:13:32 -0400

    Templates support different scenarios

--- public/script/app.js
+++ public/script/app.js
@@ -1,16 +1,19 @@
 var app = angular.module('argue', []);
 app.controller('pick_a_side', function($scope, $http) {
-  $http.get('/scenario/industrialists').success(function(data) {
+  scenarioId = window.location.href.split('/')[3]
+  $http.get('/scenario/' + scenarioId).success(function(data) {
     $scope.item = data;
   });
 });
 
 app.controller('categorize', function($scope, $http) {
-  $http.get('/scenario/industrialists').success(function(data) {
+  scenarioId = window.location.href.split('/')[3]
+  $http.get('/scenario/' + scenarioId).success(function(data) {
     $scope.item = data;
   });
 });
 
 
 Zepto(function($) {
+    /* categorize and then industrialists */ 
 });
