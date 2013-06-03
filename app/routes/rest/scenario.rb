7ebc7fad53b5df37d5f115583c01768ac0427cc8
mil <miles.sandlar@gmail.com>
2013-06-02 22:26:56 -0400

    allow upload of pdfs for scenarios

--- app/routes/rest/scenario.rb
+++ app/routes/rest/scenario.rb
@@ -13,12 +13,12 @@ put '/scenario/:uid' do
   )
 
   scn['articles'].each do |article|
-     scenario.articles << Article.create(
-       :pdf_filename => article['pdf_filename'],
-       :title       => article['title'],
-       :description => article['description'],
-       :side        => article['side']
-     )
+    scenario.articles << Article.create(
+      :pdf_filename => article['pdf_filename'],
+      :title       => article['title'],
+      :description => article['description'],
+      :side        => article['side']
+    )
   end
 
   scenario.save
@@ -38,4 +38,28 @@ end
 
 delete '/screnario/:uid' do
 
+end
+
+
+put '/scenario/:uid/:file' do
+  scenario = Scenario.all(:uid => params[:uid])
+  puts "THE FILE"
+  #puts params['file'][:tempfile].path
+  if scenario.nil? then
+    status 404
+  else
+    status 200
+    upload_directory = "#{settings.root}/data/scenarios/#{params[:uid]}/"
+    FileUtils.mkdir_p(upload_directory) unless File.exists?(upload_directory)
+    File.open("#{upload_directory}/#{params[:file]}", 'w+') do |file|
+      file.write request.body.read
+      status 200
+    end
+  end
+end
+
+get '/scenario/:uid/:file' do
+  file_path = "#{settings.root}/data/scenarios/#{params[:uid]}/#{params[:file]}"
+
+  send_file file_path, :filename => params[:file], :type => 'application/pdf'
 end
